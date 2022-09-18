62e411861e877a461162d4707421c0e139d07d46
Markus Zoppelt <markus@zoppelt.net>
2022-09-18 14:41:19 +0200

    CLI with clap

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,43 @@
+use crate::position::portfolio_position::from_file;
+use crate::position::portfolio_position::handle_position;
+use clap::{arg, Command};
+
 mod position;
+
+fn cli() -> Command<'static> {
+    Command::new("portfolio_rs")
+        .about("A simple portfolio tool")
+        .author("Markus Zoppelt")
+        .arg_required_else_help(true)
+        .allow_external_subcommands(true)
+        .subcommand(
+            Command::new("balances")
+                .about("Show the current balances of your portfolio")
+                .arg(arg!(<FILE> "JSON file with your positions"))
+                .arg_required_else_help(true),
+        )
+}
 
 #[tokio::main]
 async fn main() {
-    let filename = "example_data.json";
-    let positions = position::portfolio_position::from_file(filename);
+    let matches = cli().get_matches();
 
-    // move tasks into the async closure passed to tokio::spawn()
-    let tasks: Vec<_> = positions
-        .into_iter()
-        .map(move |mut position| {
-            tokio::spawn(async move {
-                position::portfolio_position::handle_position(&mut position).await;
+    if let Some(matches) = matches.subcommand_matches("balances") {
+        let filename = matches.value_of("FILE").expect("Cannot read file");
+        let positions = from_file(filename);
+
+        // move tasks into the async closure passed to tokio::spawn()
+        let tasks: Vec<_> = positions
+            .into_iter()
+            .map(move |mut position| {
+                tokio::spawn(async move {
+                    handle_position(&mut position).await;
+                })
             })
-        })
-        .collect();
+            .collect();
 
-    for task in tasks {
-        task.await.unwrap();
+        for task in tasks {
+            task.await.unwrap();
+        }
     }
 }
