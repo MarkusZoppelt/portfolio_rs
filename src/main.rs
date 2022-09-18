6357d1ea6e3874e73c2cff94944cb65e1d3991d9
Markus Zoppelt <markus@zoppelt.net>
2022-09-18 04:04:40 +0200

    parsing positions from JSON

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,70 @@
+use serde::Deserialize;
+use std::fs::File;
+use std::io::Read;
 use yahoo_finance_api as yahoo;
 
-// Get the latest price for a symbol
-async fn get_quote_price(symbol: &str) -> Result<yahoo::YResponse, yahoo::YahooError> {
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+struct PortfolioPosition {
+    name: String,
+    ticker: String,
+    asset_class: String,
+    amount: f64,
+
+    #[serde(skip_deserializing)]
+    last_spot: f64,
+}
+
+impl PortfolioPosition {
+    fn update_price(&mut self, last_spot: f64) {
+        self.last_spot = last_spot;
+    }
+}
+
+fn get_positions_from_file(filename: &str) -> Vec<PortfolioPosition> {
+    let mut file = File::open(filename).expect("file not found");
+    let mut data = String::new();
+    file.read_to_string(&mut data)
+        .expect("something went wrong reading the file");
+    let json =
+        serde_json::from_str::<Vec<PortfolioPosition>>(&data).expect("JSON was not well-formatted");
+    json
+}
+
+// Get the latest price for a ticker
+async fn get_quote_price(ticker: &str) -> Result<yahoo::YResponse, yahoo::YahooError> {
     yahoo::YahooConnector::new()
-        .get_latest_quotes(&symbol, "1d")
+        .get_latest_quotes(&ticker, "1d")
         .await
 }
 
 #[tokio::main]
 async fn main() {
-    let test_symbols = vec![
-        String::from("AAPL"),
-        String::from("MSFT"),
-        String::from("GOOG"),
-        String::from("AMZN"),
-    ];
+    let filename = "example_data.json";
+    let positions = get_positions_from_file(filename);
 
     // move tasks into the async closure passed to tokio::spawn()
-    let tasks: Vec<_> = test_symbols
+    let tasks: Vec<_> = positions
         .into_iter()
-        .map(|symbol| {
+        .map(move |mut position| {
             tokio::spawn(async move {
-                let quote = get_quote_price(&symbol).await;
+                let quote = get_quote_price(&position.ticker).await;
                 match quote {
                     Ok(quote) => {
-                        println!("{}: {}", symbol, quote.last_quote().unwrap().close);
+                        position.update_price(quote.last_quote().unwrap().close);
+                        println!(
+                            "{}: {} {}",
+                            position.name,
+                            position.last_spot * position.amount,
+                            position.asset_class
+                        );
                     }
                     Err(e) => {
-                        println!("Error: {}", e);
+                        if position.ticker == "-" {
+                            println!("Cash: {}", position.amount);
+                        } else {
+                            panic!("Error: {}", e);
+                        }
                     }
                 }
             })
