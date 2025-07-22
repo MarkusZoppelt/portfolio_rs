3bc8c9f185d5c551562d7cfa73440e45ecd6ac47
Markus Zoppelt <markus@zoppelt.net>
2025-07-22 10:57:46 +0200

    build: add nix flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  
+  outputs = { nixpkgs, ... }:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+    in {
+      devShells = forAllSystems (system:
+        let pkgs = nixpkgs.legacyPackages.${system};
+        in {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              rustc
+              cargo
+              libiconv
+            ];
+          };
+        }
+      );
+    };
+}
